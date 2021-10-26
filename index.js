5055e4d459c3b78362c217f814f787d1d40a8845
rahulsolanki001 <43613414+rahulsolanki001@users.noreply.github.com>
2021-10-27 03:17:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function  show1() {
+    var click=document.getElementById('hidden1');
+    click.style.display = (click.style.display != 'none') ? 'none' : 'block';
+    //click.classList.toggle('hidden')
+
+};
+
+function  show2() {
+    var click=document.getElementById('hidden2');
+    click.style.display = (click.style.display != 'none') ? 'none' : 'block';
+    //click.classList.toggle('hidden')
+
+};
+function  show3() {
+    var click=document.getElementById('hidden3');
+    click.style.display = (click.style.display != 'none') ? 'none' : 'block';
+    //click.classList.toggle('hidden')
+
+};
+function  show4() {
+    var click=document.getElementById('hidden4');
+    click.style.display = (click.style.display != 'none') ? 'none' : 'block';
+    //click.classList.toggle('hidden')
+
+};
+function  show5() {
+    var click=document.getElementById('hidden5');
+    click.style.display = (click.style.display != 'none') ? 'none' : 'block';
+    //click.classList.toggle('hidden')
+
+};
+//var e1=document.getElementsByClassName('visible')
+//e1.addEventListener('click',show)
